3f83951d2cd55a69ed492694d9497da5d6d28123
gbenhaim <gbenhaim@redhat.com>
2022-08-17 15:12:58 +0300

    simple server
    Signed-off-by: gbenhaim <gbenhaim@redhat.com>

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there %s", r.URL.Path[1:])
+}
+
 func main() {
-	for {
-		fmt.Println("Hello, world.")
-		time.Sleep(3 * time.Second)
-	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	port := ":8080"
+	log.Printf("Listening on %s", port)
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
